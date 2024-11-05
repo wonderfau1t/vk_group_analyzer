ae936d657cc4f2824ffbafeb817fcee950a974f0
ArtemiySviridov <138183223+ArtemiySviridov@users.noreply.github.com>
2024-11-05 15:52:54 +0700

    add search for all groups and filters for administered and subscribed

--- vk-miniapp/mini-app/src/panels/Home.js
+++ vk-miniapp/mini-app/src/panels/Home.js
@@ -1,11 +1,39 @@
-import { Panel, PanelHeader, Group, Cell, Div, Avatar, Search, Separator, SegmentedControl, Title, Spacing, CustomScrollView, List } from '@vkontakte/vkui';
+import { Panel, PanelHeader, Group, Cell, Div, Avatar, Search, Separator, SegmentedControl, Title, Spacing, CustomScrollView, List, Placeholder } from '@vkontakte/vkui';
 import bridge from '@vkontakte/vk-bridge';
 import { useState, useEffect } from 'react';
+import {Icon56InfoOutline} from '@vkontakte/icons';
 
 export const Home = ({ id, fetchedUser, fetchedToken }) => {
   const [subscribedGroups, setSubscribedGroups] = useState([]);
   const [administeredGroups, setAdministeredGroups] = useState([]);
   const [selectedTab, setSelectedTab] = useState('subscribes');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchedGroups, setSearchedGroups] = useState([]);
+
+  const searchAllGroups = async (query) => {
+    const response = await bridge.send("VKWebAppCallAPIMethod", {
+      method: 'groups.search',
+      params: {
+        q: query,
+        count: 200,
+        access_token: fetchedToken.access_token,
+        v: "5.199",
+      },
+    });
+    setSearchedGroups(response.response.items);
+  };
+
+  useEffect(() => {
+    if (selectedTab === 'all' && searchQuery) {
+      searchAllGroups(searchQuery);
+    }
+  }, [searchQuery, selectedTab]);
+
+  const onSearchChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    searchAllGroups(query);
+  }
 
   useEffect(() => {
     const fetchAndSplitGroups = async () => {
@@ -31,8 +59,12 @@ export const Home = ({ id, fetchedUser, fetchedToken }) => {
     setSelectedTab(value);
   };
 
-  const groupsToDisplay = selectedTab === 'subscribes' ? subscribedGroups : administeredGroups;
-
+  const groupsToDisplay =
+    selectedTab === 'subscribes'
+    ? subscribedGroups.filter((group) => group.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()))
+    : selectedTab === 'administered'
+    ? administeredGroups.filter((group) => group.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()))
+    : searchedGroups;
   return (
     <Panel id={id}>
       <PanelHeader>Анализ групп</PanelHeader>
@@ -42,7 +74,7 @@ export const Home = ({ id, fetchedUser, fetchedToken }) => {
             <Separator/>
           </Spacing>
           <Div>
-            <Search />
+            <Search value={searchQuery} onChange={onSearchChange} placeholder='Введите название группы'/>
             <Div>
               <SegmentedControl
                 name='groups'
@@ -55,6 +87,10 @@ export const Home = ({ id, fetchedUser, fetchedToken }) => {
                     label: 'Управляемые',
                     value: 'administered',
                   },
+                  {
+                    label: 'Поиск по всем группам',
+                    value: 'all',
+                  },
                 ]}
                 onChange={handleTabChange}
               />
@@ -62,19 +98,25 @@ export const Home = ({ id, fetchedUser, fetchedToken }) => {
           </Div>
           <Div>
             <Group>
-              <CustomScrollView windowResize={true} style={{height: 350}}>
-                <List>
-                  {groupsToDisplay.map((group) => (
-                    <Cell
-                      key={group.id}
-                      before={group.photo_100 ? <Avatar src={group.photo_100} /> : null}
-                      subtitle={group.activity}
-                    >
-                      {group.name}
-                    </Cell>
-                  ))}
-                </List>
-              </CustomScrollView>
+              {groupsToDisplay.length > 0 ? (
+                <CustomScrollView windowResize={true} style={{height: 350}}>
+                    <List>
+                      {groupsToDisplay.map((group) => (
+                        <Cell
+                          key={group.id}
+                          before={group.photo_100 ? <Avatar src={group.photo_100} /> : null}
+                          subtitle={group.activity}
+                        >
+                          {group.name}
+                        </Cell>
+                      ))}
+                    </List>
+                </CustomScrollView>
+              ) : (
+                <Placeholder icon={<Icon56InfoOutline/>} header='Нет групп, соответствующих вашему запросу'>
+                  Перейдите во вкладку «Поиск по всем группам» и попробуйте найти там
+                </Placeholder>
+              )}
             </Group>
           </Div>
         </Group>
