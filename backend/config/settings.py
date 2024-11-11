434bf7dfca2beb702126dae260732bd06582135a
wonderfau1t <wonderrfau1t@gmail.com>
2024-11-11 20:15:01 +0700

    fix CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",
