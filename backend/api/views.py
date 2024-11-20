254e3180b5b8ca9ddbc2810c23e7fa8a4bbc42d4
wonderfau1t <wonderrfau1t@gmail.com>
2024-11-20 10:47:01 +0700

    fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -34,34 +34,35 @@ def generate_response(group_info: GroupInfo) -> APIResponse:
                     title=messages['average_time_between_posts']['title'],
                     description=value['error_message']
                 ))
-            avegarage_time_between_posts_in_hours = value['days'] * 24 + value['hours'] + value['minutes'] / 60
-            if avegarage_time_between_posts_in_hours < 6:
-                normal.append(Parameter(
-                    id=field,
-                    title=messages['average_time_between_posts']['title'],
-                    description=messages['average_time_between_posts']['low'].format(int(value['days']),
-                                                                                     int(value['hours']),
-                                                                                     int(value['minutes']))
-                ))
-                score += 3.85
-            elif 6 < avegarage_time_between_posts_in_hours < 30:
-                good.append(Parameter(
-                    id=field,
-                    title=messages['average_time_between_posts']['title'],
-                    description=messages['average_time_between_posts']['medium'].format(int(value['days']),
-                                                                                        int(value['hours']),
-                                                                                        int(value['minutes']))
-                ))
-                score += 7.69
             else:
-                normal.append(Parameter(
-                    id=field,
-                    title=messages['average_time_between_posts']['title'],
-                    description=messages['average_time_between_posts']['high'].format(int(value['days']),
-                                                                                      int(value['hours']),
-                                                                                      int(value['minutes']))
-                ))
-                score += 0
+                avegarage_time_between_posts_in_hours = value['days'] * 24 + value['hours'] + value['minutes'] / 60
+                if avegarage_time_between_posts_in_hours < 6:
+                    normal.append(Parameter(
+                        id=field,
+                        title=messages['average_time_between_posts']['title'],
+                        description=messages['average_time_between_posts']['low'].format(int(value['days']),
+                                                                                         int(value['hours']),
+                                                                                         int(value['minutes']))
+                    ))
+                    score += 3.85
+                elif 6 < avegarage_time_between_posts_in_hours < 30:
+                    good.append(Parameter(
+                        id=field,
+                        title=messages['average_time_between_posts']['title'],
+                        description=messages['average_time_between_posts']['medium'].format(int(value['days']),
+                                                                                            int(value['hours']),
+                                                                                            int(value['minutes']))
+                    ))
+                    score += 7.69
+                else:
+                    normal.append(Parameter(
+                        id=field,
+                        title=messages['average_time_between_posts']['title'],
+                        description=messages['average_time_between_posts']['high'].format(int(value['days']),
+                                                                                          int(value['hours']),
+                                                                                          int(value['minutes']))
+                    ))
+                    score += 0
         elif field == 'er':
             if value > 3:
                 good.append(Parameter(
